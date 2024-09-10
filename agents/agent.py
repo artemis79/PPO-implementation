e1dd9be3bd7ccb84be341aaa37849cb74385fa26
Mahshid Rahmani Hanzaki <mahshidrahmani2001@gmail.com>
2024-09-10 14:29:32 -0600

    Decouple agent from algorithm

---
+++ agents/agent.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+
+import numpy as np
+from torch.distributions.categorical import Categorical
+
+class Agent(nn.Module):
+    def __init__(self, envs):
+        super(Agent, self).__init__()
+        self.critic = nn.Sequential(
+            self.layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(), 64)),
+            nn.Tanh(),
+            self.layer_init(nn.Linear(64, 64)),
+            nn.Tanh(),
+            self.layer_init(nn.Linear(64, 1), std=1.0),
+        )
+        self.actor = nn.Sequential(
+            self.layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(), 64)),
+            nn.Tanh(),
+            self.layer_init(nn.Linear(64, 64)),
+            nn.Tanh(),
+            self.layer_init(nn.Linear(64, envs.single_action_space.n), std=0.01),
+        )
+
+    def get_value(self, x):
+        return self.critic(x)
+
+    def get_action_and_value(self, x, action=None):
+        logits = self.actor(x)
+        probs = Categorical(logits=logits)
+        if action is None:
+            action = probs.sample()
+        return action, probs.log_prob(action), probs.entropy(), self.critic(x)
+
+    def layer_init(self, layer, std=np.sqrt(2), bias_const=0.0):
+        torch.nn.init.orthogonal_(layer.weight, std)
+        torch.nn.init.constant_(layer.bias, bias_const)
+        return layer
