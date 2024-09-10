40f7d4f39f7241c04387eeadbc02a266b1604d87
Mahshid Rahmani Hanzaki <mahshidrahmani2001@gmail.com>
2024-09-10 14:13:31 -0600

    Refactor code to be more readabel

--- main_ppo.py
+++ main_ppo.py
@@ -1,4 +1,4 @@
-import argparse
+
 import os
 import random
 import time
@@ -12,120 +12,8 @@ import torch.optim as optim
 from torch.distributions.categorical import Categorical
 from torch.utils.tensorboard import SummaryWriter
 
-
-def parse_args():
-    # fmt: off
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--exp-name", type=str, default=os.path.basename(__file__).rstrip(".py"),
-        help="the name of this experiment")
-    parser.add_argument("--gym-id", type=str, default="CartPole-v1",
-        help="the id of the gym environment")
-    parser.add_argument("--learning-rate", type=float, default=2.5e-4,
-        help="the learning rate of the optimizer")
-    parser.add_argument("--seed", type=int, default=1,
-        help="seed of the experiment")
-    parser.add_argument("--total-timesteps", type=int, default=25000,
-        help="total timesteps of the experiments")
-    parser.add_argument("--torch-deterministic", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
-        help="if toggled, `torch.backends.cudnn.deterministic=False`")
-    parser.add_argument("--cuda", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
-        help="if toggled, cuda will be enabled by default")
-    parser.add_argument("--track", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
-        help="if toggled, this experiment will be tracked with Weights and Biases")
-    parser.add_argument("--wandb-project-name", type=str, default="ppo-implementation",
-        help="the wandb's project name")
-    parser.add_argument("--wandb-entity", type=str, default=None,
-        help="the entity (team) of wandb's project")
-    parser.add_argument("--capture-video", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
-        help="weather to capture videos of the agent performances (check out `videos` folder)")
-
-    # # Algorithm specific arguments
-    parser.add_argument("--num-envs", type=int, default=4,
-        help="the number of parallel game environments")
-    parser.add_argument("--num-steps", type=int, default=128,
-        help="the number of steps to run in each environment per policy rollout")
-    parser.add_argument("--anneal-lr", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
-        help="Toggle learning rate annealing for policy and value networks")
-    parser.add_argument("--gae", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
-        help="Use GAE for advantage computation")
-    parser.add_argument("--gamma", type=float, default=0.99,
-        help="the discount factor gamma")
-    parser.add_argument("--gae-lambda", type=float, default=0.95,
-        help="the lambda for the general advantage estimation")
-    parser.add_argument("--num-minibatches", type=int, default=4,
-        help="the number of mini-batches")
-    parser.add_argument("--update-epochs", type=int, default=4,
-        help="the K epochs to update the policy")
-    parser.add_argument("--norm-adv", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
-        help="Toggles advantages normalization")
-    parser.add_argument("--clip-coef", type=float, default=0.2,
-        help="the surrogate clipping coefficient")
-    parser.add_argument("--clip-vloss", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
-        help="Toggles whether or not to use a clipped loss for the value function, as per the paper.")
-    parser.add_argument("--ent-coef", type=float, default=0.01,
-        help="coefficient of the entropy")
-    parser.add_argument("--vf-coef", type=float, default=0.5,
-        help="coefficient of the value function")
-    parser.add_argument("--max-grad-norm", type=float, default=0.5,
-        help="the maximum norm for the gradient clipping")
-    parser.add_argument("--target-kl", type=float, default=None,
-        help="the target KL divergence threshold")
-    args = parser.parse_args()
-    args.batch_size = int(args.num_envs * args.num_steps)
-    args.minibatch_size = int(args.batch_size // args.num_minibatches)
-    fmt: on
-    return args
-
-
-def make_env(gym_id, seed, idx, capture_video, run_name):
-    def thunk():
-        env = gym.make(gym_id, render_mode="rgb_array")
-        env = gym.wrappers.RecordEpisodeStatistics(env)
-        if capture_video:
-            if idx == 0:
-                env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
-        # env.seed(seed)
-        env.action_space.seed(seed)
-        env.observation_space.seed(seed)
-        env.reset(seed=seed)
-        return env
-
-    return thunk
-
-
-def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
-    torch.nn.init.orthogonal_(layer.weight, std)
-    torch.nn.init.constant_(layer.bias, bias_const)
-    return layer
-
-
-class Agent(nn.Module):
-    def __init__(self, envs):
-        super(Agent, self).__init__()
-        self.critic = nn.Sequential(
-            layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(), 64)),
-            nn.Tanh(),
-            layer_init(nn.Linear(64, 64)),
-            nn.Tanh(),
-            layer_init(nn.Linear(64, 1), std=1.0),
-        )
-        self.actor = nn.Sequential(
-            layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(), 64)),
-            nn.Tanh(),
-            layer_init(nn.Linear(64, 64)),
-            nn.Tanh(),
-            layer_init(nn.Linear(64, envs.single_action_space.n), std=0.01),
-        )
-
-    def get_value(self, x):
-        return self.critic(x)
-
-    def get_action_and_value(self, x, action=None):
-        logits = self.actor(x)
-        probs = Categorical(logits=logits)
-        if action is None:
-            action = probs.sample()
-        return action, probs.log_prob(action), probs.entropy(), self.critic(x)
+from utils import parse_args, make_env
+from agents.agent import Agent
 
 
 if __name__ == "__main__":
@@ -151,9 +39,8 @@ if __name__ == "__main__":
         "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
     )
 
-   
 
-    # TRY NOT TO MODIFY: seeding
+    # Seeding
     random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
@@ -161,11 +48,13 @@ if __name__ == "__main__":
 
     device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
 
+
     # env setup
     envs = gym.vector.SyncVectorEnv(
         [make_env(args.gym_id, args.seed + i, i, args.capture_video, run_name) for i in range(args.num_envs)]
     )
     assert isinstance(envs.single_action_space, gym.spaces.Discrete), "only discrete action space is supported"
+
 
     agent = Agent(envs).to(device)
     optimizer = optim.Adam(agent.parameters(), lr=args.learning_rate, eps=1e-5)
@@ -180,15 +69,13 @@ if __name__ == "__main__":
 
 
     # TRY NOT TO MODIFY: start the game
+    episode_number = 0
     global_step = 0
     start_time = time.time()
     observation, info = envs.reset()
     next_obs = torch.Tensor(observation).to(device)
     next_done = torch.zeros(args.num_envs).to(device)
     num_updates = args.total_timesteps // args.batch_size
-
-    episode_number = 0
-    
 
     for update in range(1, num_updates + 1):
         # Annealing the rate if instructed to do so.
@@ -215,16 +102,18 @@ if __name__ == "__main__":
             rewards[step] = torch.tensor(reward).to(device).view(-1)
 
             next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor(terminated).to(device)
-            print(next_obs, reward, terminated, truncated, info)
+            # print(info)
             for key, items in info.items():
                 if key=='final_info':
                     for item in items:
                         if item and "episode" in item.keys():
                             # print(f"global_step={global_step}, episodic_return={item['episode']['r']}")
                             writer.add_scalar("charts/episodic_return", item["episode"]["r"], global_step)
+                            writer.add_scalar("charts/episodic_return_per_episode", item["episode"]["r"], episode_number)
                             writer.add_scalar("charts/episodic_length", item["episode"]["l"], global_step)
+                            writer.add_scalar("charts/episodic_length_per_episode", item["episode"]["l"], episode_number)
                             # print(f"episodic_return={item['episode']['r']}")
-                            
+                            episode_number += 1
                             break
 
         # bootstrap value if not done
@@ -336,3 +225,5 @@ if __name__ == "__main__":
 
     envs.close()
     writer.close()
+
+
--- visualize.py
+++ visualize.py
@@ -1,51 +1,34 @@
-import os
-from collections import defaultdict
+import pandas as pd
+import wandb
 
-import numpy as np
-# import tensorflow as tf
-from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+api = wandb.Api()
+entity, project = "university-alberta", "ppo-implementation"
+runs = api.runs(entity + "/" + project)
 
+summary_list, config_list, name_list = [], [], []
+index = 5
+episodic_return = []
+for run in runs:
+    # .summary contains output keys/values for
+    # metrics such as accuracy.
+    #  We call ._json_dict to omit large files
+    print(run.history())
+    index += 1
+    if index > 6:
+        break
+    summary_list.append(run.summary._json_dict)
 
-def tabulate_events(dpath):
+    # .config contains the hyperparameters.
+    #  We remove special values that start with _.
+    config_list.append({k: v for k, v in run.config.items() if not k.startswith("_")})
 
-    summary_iterators = [EventAccumulator(os.path.join(dpath, dname)).Reload() for dname in os.listdir(dpath)]
+    # .name is the human-readable name of the run.
+    name_list.append(run.name)
 
-    tags = summary_iterators[0].Tags()['scalars']
+runs_df = pd.DataFrame(
+    {"summary": summary_list, "config": config_list, "name": name_list}
+)
+# print(summary_list[1])
+# print(name_list[1])
 
-    for it in summary_iterators:
-        print(tags)
-        print(it.Tags()['scalars'])
-        assert it.Tags()['scalars'] == tags
-
-    out = defaultdict(list)
-
-    for tag in tags:
-        for events in zip(*[acc.Scalars(tag) for acc in summary_iterators]):
-            # assert len(set(e.step for e in events)) == 1
-
-            out[tag].append([e.value for e in events])
-
-    return out['charts/episodic_return']
-
-
-def write_combined_events(dpath, d_combined, dname='combined'):
-
-    fpath = os.path.join(dpath, dname)
-    writer = tf.summary.FileWriter(fpath)
-
-    tags, values = zip(*d_combined.items())
-
-    timestep_mean = np.array(values).mean(axis=-1)
-
-    for tag, means in zip(tags, timestep_mean):
-        for i, mean in enumerate(means):
-            summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=mean)])
-            writer.add_summary(summary, global_step=i)
-
-        writer.flush()
-
-dpath = 'runs/'
-
-d = tabulate_events(dpath)
-
-# write_combined_events(dpath, d)
+# runs_df.to_csv("project.csv")
