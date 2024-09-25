14e3a8cdaecdc8cdeae1a571d6b35be4d9bc07e5
Mahshid Rahmani Hanzaki <mahshidrahmani2001@gmail.com>
2024-09-24 22:13:01 -0600

    Add compute canada script

--- scripts/classic_control.sh
+++ scripts/classic_control.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
+#SBATCH --account=def-mbowling
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=8G
+#SBATCH --time=0-2:59
+#SBATCH --cpu-freq=Performance
+#SBATCH --array=1-50
 
-for SEED in {1..50}
-do
-    python3 main_ppo_count.py --gym-id "CartPole-v1" --track --seed $SEED --cuda False --total-timesteps 500000 --wandb-project-name "ppo-count"
-done
+module load python/3.10.13
+if [ "$SLURM_TMPDIR" == "" ]; then
+    exit 1
+fi
+
+cd $SLURM_TMPDIR
+
+
+virtualenv pyenv
+source pyenv/bin/activate
+
+pip install 'requests[socks]' --no-index
+pip install -r /home/mrahmani/scratch/requirements.txt
+
+
+echo "Cloning repo..."
+git config --global http.proxy 'socks5://127.0.0.1:8888'
+git clone --quiet git@github.com:artemis79/PPO-implementation.git
+
+cd PPO-implementation/
+
+
+python3 main_ppo.py --gym-id "MountainCar-v0" --track --seed 1 --cuda False --total-timesteps 500000 --wandb-project-name "ppo-occupancy"
