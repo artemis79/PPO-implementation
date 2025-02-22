e9157b6c3ad4748e03ea04482c218faf24b869e1
Mahshid Rahmani Hanzaki <mahshidrahmani2001@gmail.com>
2025-02-21 18:27:26 -0700

    Fix logging observation in wandb

--- main_ppo_count.py
+++ main_ppo_count.py
@@ -266,11 +266,12 @@ if __name__ == "__main__":
         b_values = values.reshape(-1)
 
         # Log observations and rewards
+        # print('x_position', b_obs[0][0], 'velocity', b_obs[0][1])
         if args.track:
             run.log({
-                "x_position": observation[0],
-                "velocity": observation[1],
-                "step": global_step,
+                "x_position": b_obs[0][0],
+                "velocity": b_obs[0][1],
+                "global_step": global_step,
             })
         # logger.log_observation(b_obs, update)
         # logger.log_rewards(b_rewards, update)
